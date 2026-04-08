60220dbfe6cdf88b058d95f8cd2b3ff4e1c6d9b4
Kevin Anderson <kraatw2008@yahoo.com>
2026-04-08 00:08:58 -0500

    screen out privates and unknowns too

--- CallBlock/CallBlock/Platforms/Android/ScreeningService .cs
+++ CallBlock/CallBlock/Platforms/Android/ScreeningService .cs
@@ -1,10 +1,8 @@
 ﻿using Android.App;
 using Android.Provider;
 using Android.Telecom;
-using Microsoft.Maui.ApplicationModel.Communication;
 using System.Diagnostics;
-using System.Linq;
-using Android.Net;
+using Android.Database;
 
 namespace CallBlock;
 
@@ -15,22 +13,28 @@ public class ScreeningService : CallScreeningService
     public override void OnScreenCall(Call.Details callDetails)
     {
         var handle = callDetails.GetHandle();
-        if (handle == null) return;
-        var number = handle.SchemeSpecificPart;
-        Debug.WriteLine("### call received from " + number);
-        var uri = Android.Net.Uri.WithAppendedPath(ContactsContract.PhoneLookup.ContentFilterUri,
-            Android.Net.Uri.Encode(callDetails.GetHandle().SchemeSpecificPart));
-        var name = "?";
+        ICursor contact = null;
 
-        Debug.WriteLine("### uri for search: " + uri);
+        if (handle != null)
+        {
+            var number = handle.SchemeSpecificPart;
+            Debug.WriteLine("### call received from " + number);
+            var uri = Android.Net.Uri.WithAppendedPath(ContactsContract.PhoneLookup.ContentFilterUri,
+                Android.Net.Uri.Encode(callDetails.GetHandle().SchemeSpecificPart));
+            var name = "?";
 
-        var contact = ContentResolver.Query(uri,
-            [BaseColumns.Id, ContactsContract.PhoneLookup.InterfaceConsts.DisplayName], null, null, null);
+            Debug.WriteLine("### uri for search: " + uri);
 
-        Debug.WriteLine("### query succeeded");
+            contact = ContentResolver.Query(uri,
+                [BaseColumns.Id, ContactsContract.PhoneLookup.InterfaceConsts.DisplayName], null, null, null);
+
+            Debug.WriteLine("### query succeeded");
+        }
 
         var response = new CallResponse.Builder();
-        if (contact == null || contact.Count == 0)
+        if (handle == null || 
+            contact == null || 
+            contact.Count == 0)
         {
             Debug.WriteLine("### no contacts found!");
             response.SetDisallowCall(true);
